a939aa97574de24b178ada7a554eda56fcf3f3be
win1 <muravlevars@gmail.com>
2019-05-02 17:12:22 +0300

    Add Classificator model.

---
+++ src/main/java/com/murk/dk021/core/model/Classificator.java
@@ -0,0 +1,18 @@
+package com.murk.dk021.core.model;
+
+import lombok.Data;
+
+@Data
+public class Classificator {
+    private int id;
+    private Integer parentId;
+    private short num;
+    private String name;
+
+    public Classificator(int id,short num, Integer parentId,  String name) {
+        this.id = id;
+        this.parentId = parentId;
+        this.num = num;
+        this.name = name;
+    }
+}
