11d203271e4e30713608e7daf79afbcbb975ee07
win1 <muravlevars@gmail.com>
2019-05-03 12:04:29 +0300

    Add multiple validate code in VAlidation Util.

--- src/main/java/com/murk/dk021/core/utils/ValidationUtil.java
+++ src/main/java/com/murk/dk021/core/utils/ValidationUtil.java
@@ -4,6 +4,8 @@ package com.murk.dk021.core.utils;
 import com.murk.dk021.core.exception.ExceptionHelper;
 import com.murk.dk021.core.exception.NotValidCodeException;
 
+import java.util.Set;
+
 public class ValidationUtil {
     private static final String CODE_REGEXP = "^([0-9]){8}-[0-9]$";
 
@@ -15,4 +17,8 @@ public class ValidationUtil {
         }
     }
 
+    public static void validateCode(Set<String> codes)
+    {
+        codes.forEach(ValidationUtil::validateCode);
+    }
 }
