c301a552145bae61a1ea377e51c9a41184030ef8
win1 <muravlevars@gmail.com>
2019-05-03 12:03:14 +0300

    Implement update method to Classificator Service.

--- src/main/java/com/murk/dk021/core/service/ClassificatorServiceImpl.java
+++ src/main/java/com/murk/dk021/core/service/ClassificatorServiceImpl.java
@@ -1,44 +1,117 @@
 package com.murk.dk021.core.service;
 
+import com.murk.dk021.core.dao.ClassificatorDao;
+import com.murk.dk021.core.exception.NotFoundClassificatorException;
+import com.murk.dk021.core.model.Classificator;
+import com.murk.dk021.core.process.UpdateClassificatorProcess;
+import com.murk.dk021.core.utils.reader.ClassificatorReader;
 import com.murk.dk021.core.to.ClassificatorTO;
 import com.murk.dk021.core.to.STATUS;
 import com.murk.dk021.core.to.UpdateInfoTO;
+import com.murk.dk021.core.utils.converter.ClassificatorConverter;
 import com.murk.dk021.core.utils.ValidationUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
+
+import javax.annotation.PreDestroy;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.murk.dk021.core.exception.ExceptionHelper.NOT_FOUND_CLASSIFICATOR_MESSAGE;
 
 @Service
 @Slf4j
 public class ClassificatorServiceImpl implements ClassificatorService {
 
+    private ClassificatorDao dao;
+    private ClassificatorReader classificatorReader;
+    private ThreadPoolTaskExecutor updatePool;
+    private UpdateClassificatorProcess updateThread;
+    private ClassificatorConverter converter;
+
+    @Autowired
+    public ClassificatorServiceImpl(ClassificatorDao dao, ClassificatorReader classificatorReader, ThreadPoolTaskExecutor updatePool, ClassificatorConverter converter) {
+        this.dao = dao;
+        this.classificatorReader = classificatorReader;
+        this.updatePool = updatePool;
+        this.converter = converter;
+    }
+
     @Override
     public ClassificatorTO get(String code) {
+        ClassificatorTO classificatorTO;
         ValidationUtil.validateCode(code);
         log.info("Get classificator, code = {}",code);
-        return new ClassificatorTO("code1","name1");
+
+        int id = getIdFromCode(code);
+        Classificator classificator = dao.get(id);
+        if (classificator != null)
+        {
+            classificatorTO = converter.convert(classificator);
+        } else
+            {
+                throw new NotFoundClassificatorException(String.format(NOT_FOUND_CLASSIFICATOR_MESSAGE,code));
+
+            }
+
+        return classificatorTO;
     }
+
+
 
     @Override
     public Set<ClassificatorTO> getNodes(String code) {
+        Set<ClassificatorTO> classificatorsTo;
+
         ValidationUtil.validateCode(code);
+        int id = getIdFromCode(code);
 
         log.info("Get classificator nodes, code = {}",code);
 
-        Set<ClassificatorTO> classificators = new HashSet<>();
-        ClassificatorTO classificator1 = new ClassificatorTO("code1","name1");
-        ClassificatorTO classificator2 = new ClassificatorTO("code2","name2");
-        classificators.add(classificator1);
-        classificators.add(classificator2);
-        return classificators;
+        Set<Classificator> classificators= dao.getNodes(id);
+        if (classificators != null && classificators.size()>0)
+        {
+            classificatorsTo = classificators
+                    .stream()
+                    .map(converter::convert)
+                    .collect(Collectors.toSet());
+        } else
+        {
+            throw new NotFoundClassificatorException(String.format(NOT_FOUND_CLASSIFICATOR_MESSAGE,code));
+        }
+
+        return classificatorsTo;
     }
 
-    public UpdateInfoTO update()
+    private int getIdFromCode(String code) {
+        return Integer.parseInt(code.split("-")[0]);
+    }
+
+    public synchronized UpdateInfoTO update()
     {
-        log.warn("Start update classificators");
 
-        return new UpdateInfoTO(STATUS.SUCCESS,"success update");
+        UpdateInfoTO responseUpdate;
+        if (updateThread == null || updateThread.isFinish())
+        {
+            log.warn("Start update classificators");
+            updateThread = new UpdateClassificatorProcess(classificatorReader,dao,converter);
+            updatePool.execute(updateThread);
+            responseUpdate = new UpdateInfoTO(STATUS.SUCCESS,"Start update classificators");
+        } else
+            {
+                log.warn("Another update process is running");
+                responseUpdate =  new UpdateInfoTO(STATUS.FAIL,"Another update process is running");
+            }
+            return responseUpdate;
     }
+
+    @PreDestroy
+    private void closeUpdatePool()
+    {
+            updatePool.shutdown();
+    }
+
 }
